d610e80cbacd01f134467bc857bf22bf964fb3b3
vmalarcon <vmalarcon@gmail.com>
2013-08-14 19:59:47 +0000

    Added some more solutions

--- src/Scala99.scala
+++ src/Scala99.scala
@@ -75,4 +75,82 @@ object P05 {
   def reverseFunctional[A](ls: List[A]): List[A] =
     ls.foldLeft(List[A]()) { (r, h) => h :: r }
 }
+
+object P06 {
+    def isPalindrome[A](l: List[A]): Boolean = l == l.reverse
+    
+    // Not any better
+    def isPalindromeR[A](l: List[A]): Boolean = {
+        def isPalinRecursive(l: List[A], r: List[A]): Boolean = (l, r) match {
+            case (Nil, Nil)           => true
+            case (Nil, _)             => false
+            case (_, Nil)             => false
+            case (hl :: htail, rList) => hl == rList.last && isPalinRecursive(htail, rList.dropRight(1)) 
+        }
+        isPalinRecursive(l, l)
+    }
+}
 	
+object P07 {
+    def flatten(l: List[Any]): List[Any] = l flatMap {
+        case ms: List[_] => flatten(ms)
+        case e           => List(e)
+    }
+}
+
+object P08 {
+    // my version
+    def compress[A](l: List[A]): List[A] = {
+        def compressInternal(l: List[A], result: List[A]): List[A] = {
+            if (l.isEmpty) return result
+            
+            if (result.isEmpty || l.head != result.head) compressInternal(l.tail, l.head :: result)
+            else compressInternal(l.tail, result)
+        }
+        
+        compressInternal(l, Nil).reverse
+    }
+    
+    // Standard recursive.
+    def compressRecursive[A](ls: List[A]): List[A] = ls match {
+        case Nil       => Nil
+        case h :: tail => h :: compressRecursive(tail.dropWhile(_ == h))
+    }
+
+    // Tail recursive.
+    def compressTailRecursive[A](ls: List[A]): List[A] = {
+        def compressR(result: List[A], curList: List[A]): List[A] = curList match {
+            case h :: tail => compressR(h :: result, tail.dropWhile(_ == h))
+            case Nil       => result.reverse
+        }
+        compressR(Nil, ls)
+    }
+
+    // Functional.
+    def compressFunctional[A](ls: List[A]): List[A] =
+        ls.foldRight(List[A]()) { (h, r) =>
+            if (r.isEmpty || r.head != h) h :: r
+            else r
+    }
+    
+    def compressFunc[A](l: List[A]): List[A] = 
+        l.foldRight(List[A]()) { (h, r) =>
+            if (r.isEmpty || r.head != h) h :: r
+            else r
+        }
+}
+
+object P09 {
+    def pack[A](l: List[A]): List[List[A]] = l match {
+        case Nil       => Nil
+        case h :: tail => List(l.takeWhile(_ == h)) ::: pack(l.dropWhile(_ == h))
+    }
+}
+
+object P10 {
+    def encode[A](l: List[A]): List[(Int, A)] = {
+        P09.pack(l).map { e => (e.length, e.head) }
+    }
+}
+
+
